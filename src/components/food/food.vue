fdfa2f63da31d2edfa90ac36ccea1952999cd549
杨家馨 <yjx@9elephas.com>
2017-05-02 11:18:56 +0800

    时间选择器

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -3,13 +3,19 @@
     <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
       <el-form :inline="true">
         <el-form-item>
-          <el-input placeholder="姓名"></el-input>
+          <el-input placeholder="姓名" icon="search"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" >查询</el-button>
+          <el-button type="primary">查询</el-button>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" >新增</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-date-picker
+        v-model="value1"
+        placeholder="选择日期时间">
+        </el-date-picker>
         </el-form-item>
       </el-form>
     </el-col>
