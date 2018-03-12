f3db3ce6a92241c37c85415c99def2823f718f21
杨家馨 <578755236@qq.com>
2018-03-12 19:59:13 +0800

    国际化

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
+// import locale from 'element-ui/lib/locale/lang/en'
 
 import '@/styles/index.scss' // global css
 
@@ -13,7 +13,7 @@ import router from './router'
 import store from './store'
 import '@/icons' // icon
 import '@/permission' // permission control
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -12,11 +12,30 @@
             </el-option>
           </el-select>
         </el-form-item>
+        <el-form-item>
+          <el-select v-model="value" clearable placeholder="状态">
+              <el-option
+                v-for="item in status"
+                :key="item.statusId"
+                :label="item.label"
+                :value="item.statusId">
+              </el-option>
+          </el-select>
+        </el-form-item>
+                <el-form-item >
+          <el-input placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item>
+           <el-button type="primary"><i class="el-icon-search"></i>搜索</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">新增</el-button>
+        </el-form-item>
       </el-form>
     </el-col>
     <!--列表-->
     <el-table :data="list" v-loading="listLoading" border element-loading-text="拼命加载中" style="width: 100%;">
-      <el-table-column prop="uid">
+      <el-table-column prop="uid" label="序号">
       </el-table-column>
       <el-table-column prop="cname" label="姓名">
       </el-table-column>
@@ -38,16 +57,25 @@
       </el-table-column>
     </el-table>
     <!--工具条-->
-    <el-col :span="24" class="toolbar">
-      <el-pagination layout="prev,total,pager, next"
+    <!-- <el-col :span="24" class="toolbar"> -->
+      <el-pagination layout="total, prev, pager, next"
                       background
-                     :page-size="20"
+                     :page-size="10"
                      @size-change="handleSizeChange"
                      :total="total"
                       @current-change="handleCurrentChange"
                      style="text-align:center;">
       </el-pagination>
-    </el-col>
+      <!-- <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[10, 20, 30]"
+        :page-size="20"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination> -->
+    <!-- </el-col> -->
   </div>
 </template>
 
@@ -60,6 +88,15 @@ export default {
       listLoading: true,
       total: 0,
       page: 1,
+      status: [
+        {
+          statusId: 1,
+          label: '启用'
+        }, {
+          statusId: 0,
+          label: '禁用'
+        }
+      ],
       options: [
         {
           value: '选项1',
@@ -96,7 +133,7 @@ export default {
     fetchData() {
       this.listLoading = true
       getList(this.listQuery).then(response => {
-        const limit = 20
+        const limit = 10
         const pageList = response.data.filter((item, index) => index < limit * this.page && index >= limit * (this.page - 1))
         console.log(pageList)
         this.total = response.data.length
