68b91e9816a757edabb51886e589ca8bf5baf692
杨家馨 <578755236@qq.com>
2018-03-12 17:31:45 +0800

    easy-mock 表格数据

--- src/api/table.js
+++ src/api/table.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    url: '/table/list',
+    url: '/user/table',
     method: 'get',
     params
   })
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,38 +1,51 @@
 <template>
   <div class="app-container">
-    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column align="center" label='ID' width="95">
+    <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+      <el-form :inline="true">
+        <el-form-item>
+          <el-select v-model="value" clearable placeholder="所属机构">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <!--列表-->
+    <el-table :data="list" v-loading="listLoading" border fit highlight-current-row element-loading-text="拼命加载中" style="width: 100%;">
+      <el-table-column type="index">
+      </el-table-column>
+      <el-table-column prop="cname" label="姓名">
+      </el-table-column>
+      <el-table-column prop="number" label="阅读数">
+      </el-table-column>
+      <el-table-column prop="date" label="时间">
+      </el-table-column>
+      <el-table-column  label="状态" width="120" >
         <template slot-scope="scope">
-          {{scope.$index}}
+           <el-button size="small" :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 1">启用</el-button>
+          <el-button size="small"  :type="scope.row.status | statusFilter" @click="isStatus(scope.$index, scope.row)" v-if="scope.row.status == 2">禁用</el-button>
         </template>
       </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{scope.row.title}}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.author}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{scope.row.pageviews}}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span>{{scope.row.display_time}}</span>
+      <el-table-column prop="operation" label="操作 ">
+        <template slot-scope="scope" >
+         <el-button size="small">编辑</el-button>
+          <el-button size="small" type="danger">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!--工具条-->
+    <el-col :span="24" class="toolbar">
+      <el-pagination layout="prev,total,pager, next"
+                     :page-size="10"
+                     @size-change="handleSizeChange"
+                     :total="total"
+                     style="text-align:center;">
+      </el-pagination>
+    </el-col>
   </div>
 </template>
 
@@ -43,29 +56,54 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
-    }
-  },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
+      listLoading: true,
+      total: 0,
+      page: 1,
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+      value: ''
     }
   },
   created() {
     this.fetchData()
   },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        1: 'success',
+        2: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
   methods: {
     fetchData() {
       this.listLoading = true
       getList(this.listQuery).then(response => {
-        this.list = response.data.items
+        this.list = response.data
+        this.total = response.totalRow
         this.listLoading = false
       })
+    },
+    handleSizeChange(val) {
+      this.page = val
+      console.log(this.page)
+      this.fetchData()
     }
   }
 }
