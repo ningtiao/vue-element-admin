882b98000b95af66d0e37f8bb17ffd9f003e8406
杨家馨 <578755236@qq.com>
2018-07-14 14:26:58 +0800

    删除数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -7,6 +7,8 @@ import transactionAPI from './transaction'
 //   timeout: '350-600'
 // })
 
+
+
 // 登录相关
 Mock.mock(/\/login\/login/, 'post', loginAPI.loginByUsername)
 Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
