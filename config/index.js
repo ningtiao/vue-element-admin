aae9ed11b13efc4fdc229940712763b2c1405699
杨家馨 <578755236@qq.com>
2018-07-19 18:35:43 +0800

    富文本

--- config/index.js
+++ config/index.js
@@ -34,10 +34,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
 
     // you can set by youself according to actual condition
